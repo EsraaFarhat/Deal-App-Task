fb737e3478d81ac7ae20495b31c46fd906419bcd
EsraaFarhat <esraafarhat97@gmail.com>
2024-05-22 23:01:47 +0300

    project setup

---
+++ src/app.mjs
@@ -0,0 +1,34 @@
+import express from "express";
+import compression from "compression";
+import helmet from "helmet";
+import cors from "cors";
+
+import "./database/connection.mjs";
+import config from "./config/config.mjs";
+import AppErrorHandler from "./config/error.mjs";
+import { morganErrorHandler, morganSuccessHandler } from "./config/morgan.mjs";
+
+const app = express();
+
+app.use(morganSuccessHandler);
+app.use(morganErrorHandler);
+app.use(cors());
+
+app.use(helmet());
+
+app.use(express.json({ limit: config.router.limit.request }));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: config.router.limit.request,
+    parameterLimit: config.router.limit.parameter,
+  })
+);
+
+app.use(compression());
+
+
+app.use(AppErrorHandler.handler);
+app.use(AppErrorHandler.notFound);
+
+export default app;
---
+++ src/database/connection.mjs
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import config from "../config/config.mjs";
+import logger from "../shared/logger.mjs";
+
+// Connect to MongoDB
+export const db = mongoose
+  .connect(config.mongoose.url, config.mongoose.options)
+  .then(() => {
+    logger.info("[MongoDB] connected");
+  })
+  .catch((err) => {
+    logger.error(`[MongoDB] Failed to connect: ${err}`);
+  });
